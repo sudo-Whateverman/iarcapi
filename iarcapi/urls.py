388388f59503d6461174e72c7bc0c9de7900392e
Nick <nick@smartgreen.co.il>
2016-12-31 13:41:43 +0200

    I think i fixed the bork i did.

---
+++ iarcapi/urls.py
@@ -0,0 +1,32 @@
+"""iarcapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from rest_framework import routers
+from questionaire import views
+
+router = routers.DefaultRouter()
+#makes sure that the API endpoints work
+router.register(r'api/questionaire', views.questionview, base_name='nick')
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^qs/$', views.questionview_list),
+    url(r'^qs/(?P<pk>[0-9]+)/$', views.questionview_detail),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
---
+++ questionaire/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from questionaire.models import examQA
+
+admin.site.register(examQA)
