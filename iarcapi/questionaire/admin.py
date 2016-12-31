c67624ed588e0761346b4ece30595d9a2242ff73
Nick <nick@smartgreen.co.il>
2016-12-31 13:00:04 +0200

    some collect static magic

--- iarcapi/questionaire/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from questionaire.models import examQA
-
-admin.site.register(examQA)
--- iarcapi/questionaire/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from questionaire.models import examQA
-
-
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = examQA
-        fields = ('id', 'Question', 'CorrectAnswer', 'AdditionalAnswer1', 'AdditionalAnswer2', 'AdditionalAnswer3')
-
